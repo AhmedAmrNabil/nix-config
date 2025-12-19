1db73182a84c5b06f9ce477c2307d1f410f1cda3
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-19 05:51:31 +0200

    fix: correct starship.toml symlink path

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -3,7 +3,7 @@
 {
 
   xdg.configFile."starship.toml" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/btngana/dotfiles/config/starship.toml";
+    source = config.lib.file.mkOutOfStoreSymlink "/home/btngana/dotfiles/config/starship/starship.toml";
   };
 
 }
