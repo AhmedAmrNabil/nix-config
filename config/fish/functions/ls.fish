c49f9d91e85881723ec5e49db5257324542df1c2
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-23 05:00:25 +0000

    fix: prepend command keyword to eza in ls function for proper execution

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls --wraps=exa --description 'alias ls exa'
-  eza --icons --hyperlink --color=always --group-directories-first $argv
+  command eza --icons --hyperlink --color=always --group-directories-first $argv
 end
