a9cbc6d467837d914a351eed2ecec7a8c971a8df
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-23 18:52:34 +0200

    feat: remove micro settings file and streamline configuration

--- home/programs/micro.nix
+++ home/programs/micro.nix
@@ -1,15 +1,11 @@
 { config, lib, ... }:
 {
-  programs.micro.enable = true;
-
-  xdg.configFile."micro/settings.json" = lib.mkForce {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/btngana/dotfiles/config/micro/settings.json";
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "catppuccin-mocha";
+    };
   };
-
-  xdg.configFile."micro/bindings.json" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/btngana/dotfiles/config/micro/bindings.json";
-  };
-
   xdg.configFile."micro/colorschemes" = {
     source = config.lib.file.mkOutOfStoreSymlink "/home/btngana/dotfiles/config/micro/colorschemes";
     recursive = true;
