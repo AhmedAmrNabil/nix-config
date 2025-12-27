e52594cde6c7da7f5dcb8311a5712b69dddd4c30
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-27 14:27:05 +0200

    fix: update syntax of local font config

--- modules/core/fonts/default.nix
+++ modules/core/fonts/default.nix
@@ -47,7 +47,7 @@ in
       };
       antialias = true;
       localConf = ''
-                <?xml version="1.0"?>
+        <?xml version="1.0"?>
         <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
         <fontconfig>
             <match target="pattern">
