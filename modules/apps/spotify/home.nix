965fc3965ffb06366ca3376ed74e08cb414f28fd
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-29 14:46:41 +0200

    style: add catppuccin colors to spotify

--- modules/apps/spotify/home.nix
+++ modules/apps/spotify/home.nix
@@ -7,6 +7,21 @@
     {
       enable = true;
       theme = spicePkgs.themes.text;
+      customColorScheme = {
+        accent = "89dceb";
+        accent-active = "89dceb";
+        accent-inactive = "1e1e2e";
+        banner = "89dceb";
+        border-active = "89dceb";
+        border-inactive = "313244";
+        header = "585b70";
+        highlight = "585b70";
+        main = "1e1e2e";
+        notification = "89b4fa";
+        notification-error = "f38ba8";
+        subtext = "a6adc8";
+        text = "cdd6f4";
+      };
       enabledExtensions = with spicePkgs.extensions; [
         adblock
         shuffle
