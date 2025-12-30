125e4292fe4542a4bc013bea4958184193ef643d
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-30 18:26:26 +0200

    feat: add Steam Gamescope session support and update desktop entry

--- modules/apps/steam/default.nix
+++ modules/apps/steam/default.nix
@@ -7,6 +7,25 @@
 }:
 let
   cfg = config.apps.steam;
+
+  steamGamescopeSession = pkgs.stdenvNoCC.mkDerivation {
+    name = "steam-gamescope-session";
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/share/wayland-sessions
+      cat > $out/share/wayland-sessions/steam-gamescope.desktop <<EOF
+      [Desktop Entry]
+      Name=Steam (Gamescope)
+      Comment=Steam Big Picture in Gamescope
+      Exec=env __GLX_VENDOR_LIBRARY_NAME=nvidia \
+        ${lib.getExe pkgs.gamescope} -W 2560 -H 1440 -r 180 -f --sdr-gamut-wideness 1 -e -- \
+        ${lib.getExe pkgs.steam} -tenfoot
+      Type=Application
+      EOF
+    '';
+
+    passthru.providedSessions = [ "steam-gamescope" ];
+  };
 in
 {
   options.apps.steam = {
@@ -22,15 +41,20 @@ in
 
     environment.systemPackages = with pkgs; [
       mangohud
+      gamescope-wsi
     ];
 
     programs.gamemode.enable = true;
+
+    services.displayManager.sessionPackages = [
+      steamGamescopeSession
+    ];
 
     home-manager.users.${username} = {
       xdg.desktopEntries.steam-gamescope = {
         name = "Steam (gamescope)";
         genericName = "Steam";
-        exec = "${lib.getExe' pkgs.gamescope "gamescope"} -e -- ${lib.getExe' pkgs.steam "steam"} -tenfoot";
+        exec = "env __GLX_VENDOR_LIBRARY_NAME=nvidia ${lib.getExe pkgs.gamescope} -W 2560 -H 1440 -r 180 -f --sdr-gamut-wideness 1 -e -- ${lib.getExe pkgs.steam} -tenfoot";
         icon = "steam";
         categories = [
           "Game"
