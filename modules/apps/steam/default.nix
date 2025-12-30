2ed22d75f4a06523e2255ec0767dae5f1b863a82
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-30 16:59:45 +0200

    feat: add Steam support with gamescope integration and necessary packages

--- modules/apps/steam/default.nix
+++ modules/apps/steam/default.nix
@@ -1,6 +1,8 @@
 {
   config,
   lib,
+  pkgs,
+  username,
   ...
 }:
 let
@@ -15,6 +17,25 @@ in
       enable = true;
       remotePlay.openFirewall = true; # For Steam Remote Play
       dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      mangohud
+    ];
+
+    programs.gamemode.enable = true;
+
+    home-manager.users.${username} = {
+      xdg.desktopEntries.steam-gamescope = {
+        name = "Steam (gamescope)";
+        genericName = "Steam";
+        exec = "${lib.getExe' pkgs.gamescope "gamescope"} -e -- ${lib.getExe' pkgs.steam "steam"} -tenfoot";
+        icon = "steam";
+        categories = [
+          "Game"
+        ];
+      };
     };
   };
 }
