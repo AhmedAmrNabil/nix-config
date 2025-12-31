0b49c33246bed2072fa60068194ebafecb11cf00
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-12-31 04:31:16 +0200

    refactor(btop): simplify bto config

--- modules/apps/btop/default.nix
+++ modules/apps/btop/default.nix
@@ -2,10 +2,17 @@
   config,
   lib,
   username,
+  pkgs,
   ...
 }:
 let
   cfg = config.apps.btop;
+  catppuccin-btop = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "btop";
+    rev = "f437574b600f1c6d932627050b15ff5153b58fa3";
+    hash = "sha256-mEGZwScVPWGu+Vbtddc/sJ+mNdD2kKienGZVUcTSl+c=";
+  };
 in
 {
   options.apps.btop = {
@@ -13,8 +20,20 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home-manager.users.${username} = {
-      imports = [ ./home.nix ];
-    };
+    home-manager.users.${username} =
+      { config, ... }:
+      {
+        programs.btop = {
+          enable = true;
+          package = pkgs.btop-cuda;
+          settings = {
+            color_theme = "${config.home.homeDirectory}/.config/btop/themes/catppuccin_mocha.theme";
+            shown_boxes = "cpu mem net proc gpu0";
+          };
+          themes = {
+            catppuccin_mocha = builtins.readFile "${catppuccin-btop}/themes/catppuccin_mocha.theme";
+          };
+        };
+      };
   };
 }
