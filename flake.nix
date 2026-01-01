3bb1e1ac5262317fafa45c4f84f1f9a24a33ac56
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-01-02 00:32:41 +0200

    refactor(flake): update nixpkgs inputs to follow nixpkgs and refactor mkSystem

--- flake.nix
+++ flake.nix
@@ -4,12 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland.url = "github:hyprwm/Hyprland";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -29,17 +40,17 @@
         nix-vscode-extensions.overlays.default
       ];
 
+      # Evaluate pkgs once and reuse
       pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      pkgsUnstable = import inputs.nixpkgs-unstable {
         inherit system overlays;
         config.allowUnfree = true;
       };
-
-      localPkgs = import ./packages {
-        inherit pkgs;
+      pkgsUnstable = import inputs.nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
       };
+
+      localPkgs = import ./packages { inherit pkgs; };
 
       username = "btngana";
 
@@ -48,53 +59,37 @@
         userEmail = "ahmedamr24680@gmail.com";
       };
 
-      mkSystem =
-        host:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/${host}/configuration.nix
-            ./modules
-            # Apply overlays to NixOS
-            { nixpkgs.overlays = overlays; }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = {
-                  inherit
-                    inputs
-                    localPkgs
-                    system
-                    pkgsUnstable
-                    gitConfig
-                    ;
-                };
-                sharedModules = with inputs; [
-                  spicetify-nix.homeManagerModules.default
-                ];
-                backupFileExtension = "hm-bak";
-                users.${username}.home.stateVersion = "25.11";
-              };
-            }
-          ]
-          ++ (if host == "wsl" then [ nixos-wsl.nixosModules.default ] else [ ]);
-          specialArgs = {
-            inherit
-              inputs
-              system
-              localPkgs
-              username
-              ;
+      commonModules = [
+        ./modules
+        { nixpkgs.overlays = overlays; }
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {
+              inherit inputs localPkgs system pkgsUnstable gitConfig;
+            };
+            sharedModules = [ inputs.spicetify-nix.homeManagerModules.default ];
+            backupFileExtension = "hm-bak";
+            users.${username}.home.stateVersion = "25.11";
           };
+        }
+      ];
+
+      specialArgs = { inherit inputs system localPkgs username; };
+
+      mkSystem = host: extraModules:
+        nixpkgs.lib.nixosSystem {
+          inherit system specialArgs;
+          modules = [ ./hosts/${host}/configuration.nix ] ++ commonModules ++ extraModules;
         };
     in
     {
       nixosConfigurations = {
-        desktop-nixos = mkSystem "desktop";
-        laptop-nixos = mkSystem "laptop";
-        wsl-nixos = mkSystem "wsl";
+        desktop-nixos = mkSystem "desktop" [];
+        laptop-nixos = mkSystem "laptop" [];
+        wsl-nixos = mkSystem "wsl" [ nixos-wsl.nixosModules.default ];
       };
     };
 }
