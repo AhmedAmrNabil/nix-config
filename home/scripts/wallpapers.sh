6ea85dbb1a207c8cda7fbb5abd55f5bb63ea7871
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-04-05 00:01:16 +0200

    fix(scripts): fix wallpaper script formatting

--- home/scripts/wallpapers.sh
+++ home/scripts/wallpapers.sh
@@ -6,7 +6,7 @@ ROFI_THEME="$HOME/dotfiles/home/apps/rofi/wallpaper.rasi"
 wallpaper=$(
     find "$WALLPAPER_DIR" -maxdepth 1 -type f  \
     -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \
-		| while read -r image; do
+    | while read -r image; do
         printf "%s\0icon\x1f%s\n" "$(basename "$image")" "$image"
     done \
     | rofi -dmenu -i -p "Wallpapers" -theme "$ROFI_THEME"
