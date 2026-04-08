6d4fc47226e6f1d8ab6571753a2a769a37d06cc0
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-04-08 23:46:28 +0200

    feat(only-office): add cambria-math font and update font installation process

--- home/apps/only-office/default.nix
+++ home/apps/only-office/default.nix
@@ -15,14 +15,15 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       onlyoffice-desktopeditors
-      localPkgs.wps-fonts
+      pkgs.vista-fonts
     ];
     home.activation.copyFontsLocalShare = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       fontDir="$HOME/.local/share/fonts"
       mkdir -p "$fontDir"
 
       install -m644 ${pkgs.corefonts}/share/fonts/truetype/* "$fontDir/"
-      install -m644 ${localPkgs.wps-fonts}/share/fonts/truetype/* "$fontDir/"
+      install -m644 ${pkgs.vista-fonts}/share/fonts/truetype/* "$fontDir/"
+      install -m644 ${./cambria-math.ttf} "$fontDir/cambria-math.ttf"
     '';
   };
 }
