3d161d45a67e87296de0758894961ef61f085e58
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-04-11 00:54:27 +0200

    fix(vscode): revert package to original vscode version

--- home/apps/vscode/default.nix
+++ home/apps/vscode/default.nix
@@ -15,7 +15,7 @@ in
   config = lib.mkIf cfg.enable {
     programs.vscode = {
       enable = true;
-      package = pkgsUnstable.vscode.fhsWithPackages (ps: with ps; [ avrdude ]);
+      package = pkgsUnstable.vscode;
       mutableExtensionsDir = true;
     };
 
