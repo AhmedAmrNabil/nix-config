5768f0ae38044cee03a400043bbab4503249d86a
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-05-11 16:04:30 +0300

    feat(gpu-screen-recorder): bump  to 1.2.3

--- packages/gpu-screen-recorder-notification/default.nix
+++ packages/gpu-screen-recorder-notification/default.nix
@@ -1,26 +1,28 @@
 {
   lib,
-  stdenv,
   fetchgit,
+  gitUpdater,
+  libglvnd,
+  makeWrapper,
   meson,
   ninja,
+  pango,
   pkg-config,
-  wayland,
+  stdenv,
   wayland-scanner,
-  libglvnd,
+  wayland,
+  gsettings-desktop-schemas,
   xorg,
-  gitUpdater,
-  makeWrapper,
 }:
 
 stdenv.mkDerivation rec {
   pname = "gpu-screen-recorder-notification";
-  version = "1.1.0";
+  version = "1.2.3";
 
   src = fetchgit {
     url = "https://repo.dec05eba.com/gpu-screen-recorder-notification";
     tag = version;
-    hash = "sha256-ODifZ046DEBNiGT3+S6pQyF8ekrb6LIHWton8nv1MBo=";
+    hash = "sha256-tzyrI4B5JWiUOpaww/2oGAvYgNKGb63eap1NKy5uysU=";
   };
 
   nativeBuildInputs = [
@@ -29,25 +31,30 @@ stdenv.mkDerivation rec {
     pkg-config
     makeWrapper
   ];
+
   buildInputs = [
     libglvnd
+    pango
     xorg.libX11
     xorg.libXrandr
     xorg.libXrender
     xorg.libXext
     wayland
     wayland-scanner
+    gsettings-desktop-schemas
   ];
 
   strictDeps = true;
 
+  mesonFlags = [
+    "--buildtype=release"
+    "-Dstrip=true"
+  ];
+
   postInstall = ''
     wrapProgram "$out/bin/${meta.mainProgram}" \
-      --prefix LD_LIBRARY_PATH : "${
-        lib.makeLibraryPath [
-          libglvnd
-        ]
-      }"
+      --prefix XDG_DATA_DIRS : "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
   '';
 
   passthru.updateScript = gitUpdater {
@@ -55,8 +62,8 @@ stdenv.mkDerivation rec {
   };
 
   meta = {
-    description = "Notification in the style of ShadowPlay";
-    homepage = "https://git.dec05eba.com/gpu-screen-recorder-notification/";
+    description = "Notification in the style of ShadowPlay.";
+    homepage = "https://git.dec05eba.com/gpu-screen-recorder-notification";
     license = lib.licenses.gpl3Only;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [
