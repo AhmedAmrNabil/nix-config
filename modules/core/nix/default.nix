b4bcd3880965d64ca219a075ba8d766e69f45054
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-05-13 18:51:13 +0300

    feat(nix-cfg): update nix settings for auto optimise

--- modules/core/nix/default.nix
+++ modules/core/nix/default.nix
@@ -13,34 +13,38 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    nix = {
-      settings = {
-        warn-dirty = false;
-        use-xdg-base-directories = true;
-        keep-going = true;
-        trusted-users = [
-          "${username}"
-          "@wheel"
-        ];
-        substituters = [
-          "https://nix-community.cachix.org"
-          "https://cache.nixos.org"
-          "https://hyprland.cachix.org"
-          "https://cache.garnix.io"
-        ];
-        trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        ];
-        trusted-substituters = ["https://hyprland.cachix.org"];
+    nix.settings = {
+      warn-dirty = false;
+      use-xdg-base-directories = true;
+      keep-going = true;
+      trusted-users = [
+        "${username}"
+        "@wheel"
+      ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+        "https://hyprland.cachix.org"
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+      trusted-substituters = [ "https://hyprland.cachix.org" ];
 
-        # enable nix flakes
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-      };
+      # enable nix flakes
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+
+    nix.optimise.automatic = true;
+    nix.gc = {
+      automatic = true;
+      dates = "daily";
     };
   };
 }
