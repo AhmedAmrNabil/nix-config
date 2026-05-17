1a5fff77ef837dbd2fa9a2b6b7655e6effdd55d1
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-05-17 18:01:03 +0300

    feat(hyprland): add hyprpaper service and enable swaync

--- home/de/hyprland/default.nix
+++ home/de/hyprland/default.nix
@@ -1,10 +1,14 @@
 {
   config,
   lib,
+  pkgs,
+  pkgsUnstable,
+  inputs,
   ...
 }:
 let
   cfg = config.de.hyprland;
+  hyprlandPackages = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
 in
 {
   options.de.hyprland = {
@@ -16,5 +20,31 @@ in
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/de/hyprland/hyprland.lua";
     };
 
+    services.hyprpaper = {
+      enable = true;
+      package = pkgsUnstable.hyprpaper;
+      settings = {
+        wallpaper = [
+          {
+            monitor = "DP-1";
+            path = "${config.home.homeDirectory}/Pictures/wallpapers/pale-mountains.jpg";
+            fit_mode = "cover";
+          }
+          {
+            monitor = "DP-2";
+            path = "${config.home.homeDirectory}/Pictures/wallpapers/pale-mountains.jpg";
+            fit_mode = "cover";
+          }
+        ];
+      };
+    };
+
+    apps.swaync.enable = true;
+
+    home.packages = with pkgs; [
+      playerctl
+      grim
+      slurp
+    ];
   };
 }
