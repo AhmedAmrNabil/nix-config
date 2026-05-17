fed07af06c78b4bf447deb7f1400311382bc75b3
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-05-17 18:23:54 +0300

    feat(swaync): add style.css

--- home/apps/swaync/default.nix
+++ home/apps/swaync/default.nix
@@ -17,5 +17,9 @@ in
     xdg.configFile."swaync/config.json" = lib.mkForce {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/apps/swaync/config.json";
     };
+
+    xdg.configFile."swaync/style.css" = lib.mkForce {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/apps/swaync/style.css";
+    };
   };
 }
