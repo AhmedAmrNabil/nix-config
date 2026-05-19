6321bdeb6fa1a5645be5b84da84d7afb4afeaaa9
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-05-20 02:21:46 +0300

    feat(bat): restructure configuration to include themes and improve readability

--- home/apps/bat/default.nix
+++ home/apps/bat/default.nix
@@ -1,17 +1,26 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.apps.bat;
+  catppuccinMochaBat = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/bat/refs/heads/main/themes/Catppuccin%20Mocha.tmTheme";
+    hash = "sha256-OVVm8IzrMBuTa5HAd2kO+U9662UbEhVT8gHJnCvUqnc=";
+  };
 in
 {
   options.apps.bat = {
     enable = lib.mkEnableOption "Bat: cat alternative with extra config";
   };
   config = lib.mkIf cfg.enable {
-    programs.bat.enable = true;
+    programs.bat = {
+      enable = true;
+      themes.catppuccin.src = catppuccinMochaBat;
+      config.theme = "catppuccin";
+    };
     home.shellAliases = {
       cat = "bat --paging=never -p";
     };
