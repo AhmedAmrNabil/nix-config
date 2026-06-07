4737b4c1a0f7d192ebe6e432c200ab785f8fafc8
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-06-07 05:40:15 +0300

    refactor(steam): remove custom Gamescope session configuration

--- modules/apps/steam/default.nix
+++ modules/apps/steam/default.nix
@@ -6,32 +6,12 @@
 }:
 let
   cfg = config.apps.steam;
-
-  steamGamescopeSession = pkgs.stdenvNoCC.mkDerivation {
-    name = "steam-gamescope-session";
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out/share/wayland-sessions
-      cat > $out/share/wayland-sessions/steam-gamescope.desktop <<EOF
-      [Desktop Entry]
-      Name=Steam (Gamescope)
-      Comment=Steam Big Picture in Gamescope
-      Exec=env __GLX_VENDOR_LIBRARY_NAME=nvidia \
-        ${lib.getExe pkgs.gamescope} -W 2560 -H 1440 -r 180 -c DP-1 -f --sdr-gamut-wideness 1 -e -- \
-        ${lib.getExe pkgs.steam} -tenfoot
-      Type=Application
-      EOF
-    '';
-
-    passthru.providedSessions = [ "steam-gamescope" ];
-  };
 in
 {
   options.apps.steam = {
     enable = lib.mkEnableOption "Steam support";
   };
   config = lib.mkIf cfg.enable {
-
     programs.gamemode.enable = true;
     programs.steam = {
       enable = true;
@@ -51,10 +31,6 @@ in
       mangohud
       gamescope-wsi
       protonup-qt
-    ];
-
-    services.displayManager.sessionPackages = [
-      steamGamescopeSession
     ];
   };
 }
