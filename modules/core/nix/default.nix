0fbd018577a9a5a7dad52ba1709feba2695669f5
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-07-07 10:39:46 +0300

    feat(nix): add extra cuda substituter

--- modules/core/nix/default.nix
+++ modules/core/nix/default.nix
@@ -27,13 +27,15 @@ in
         "https://cache.nixos.org"
         "https://hyprland.cachix.org"
         "https://cache.garnix.io"
-        "https://noctalia.cachix.org" 
+        "https://noctalia.cachix.org"
+        "https://cuda-maintainers.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
       trusted-substituters = [ "https://hyprland.cachix.org" ];
 
