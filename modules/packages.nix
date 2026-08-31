f13595c161c726505888904164556321f4cc8caa
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-08-31 09:40:18 +0300

    refactor(packages): filter derivations in package list

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,8 +1,10 @@
 {
-  perSystem = { pkgs, ... }: {
-    packages = pkgs.lib.packagesFromDirectoryRecursive {
-      inherit (pkgs) callPackage newScope;
-      directory = ../packages;
-    };
+  perSystem = { pkgs, lib, ... }: {
+    packages = lib.filterAttrs (_: v: lib.isDerivation v) (
+      lib.packagesFromDirectoryRecursive {
+        inherit (pkgs) callPackage newScope;
+        directory = ../packages;
+      }
+    );
   };
 }
