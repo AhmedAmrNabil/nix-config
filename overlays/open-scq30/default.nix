175c5f85a1c8295d5498e4f5232a1946b0aad9ca
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-09-04 00:22:54 +0300

    feat: remove unneeded open-scq30 overlay

--- overlays/open-scq30/default.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  open-scq30 = prev.open-scq30.overrideAttrs (old: {
-    preFixup = (old.preFixup or "") + ''
-      gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${prev.cosmic-icons}/share")
-    '';
-  });
-}
